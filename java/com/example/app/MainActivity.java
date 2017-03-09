0afd4307ab8ea42046e00cdb35403d8940cb2a55
abhish3k9 <abhishekuniyal09@gmail.com>
2017-03-10 02:45:51 +0530

    Initial commit

---
+++ java/com/example/app/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Use remote resource
+         mWebView.loadUrl("http://feekart.srmuniv.ac.in/srmopp/");
+
+
+        mWebView.getSettings().setDomStorageEnabled(true);
+       // webView.loadUrl("http://example.com/");
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+
+
+                final String js = "javascript:" +
+                        "document.getElementById('accountname').value = 'RA1411008020062';"  +
+                        "document.getElementById('password').value = 'chandan111';"  +
+                        "loginform()";
+
+                if (Build.VERSION.SDK_INT >= 19) {
+                    view.evaluateJavascript(js, new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String s) {
+
+                        }
+                    });
+                } else {
+                    view.loadUrl(js);
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Stop local links and redirects from opening in browser instead of WebView
+        //mWebView.setWebViewClient(new MyAppWebViewClient());
+
+        // Use local resource
+        // mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+}
